12f4c63b9d24b441814a05b7d4117c056a14ae51
mohamed essam <mmaraemmm@hotmail.com>
2025-07-27 16:36:45 +0300

    add connections

---
+++ Articalproject/Data/Connections.cs
@@ -0,0 +1,8 @@
+﻿namespace Articalproject.Data
+{
+    public class Connections
+    {
+        string conection1="Server=localhost;Database=ArticalProject;User Id=sa;Password=123456;";
+        string conection2= "Server=DESKTOP-88D8II7\\MOHAMEDSERVER;Database=MyArtical;User Id = sa;Password=1144;TrustServerCertificate=true;";
+    }
+}
