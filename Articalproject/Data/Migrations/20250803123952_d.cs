003705c90564d35841211018b5df760e231504b8
mohamed essam <mmaraemmm@hotmail.com>
2025-08-03 15:40:59 +0300

    add category

---
+++ Articalproject/Data/Migrations/20250803123952_d.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Articalproject.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class d : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "categorys",
+                newName: "NameEn");
+
+            migrationBuilder.AddColumn<string>(
+                name: "NameAr",
+                table: "categorys",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NameAr",
+                table: "categorys");
+
+            migrationBuilder.RenameColumn(
+                name: "NameEn",
+                table: "categorys",
+                newName: "Name");
+        }
+    }
+}
